7ebd9627a737db0034701cca9e08b4c994111a2c
Anuranjan Srivastav <toanuranjans@gmail.com>
2023-12-04 23:22:16 +0530

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,18 @@
 import '@/app/ui/global.css';
 import { inter } from './ui/fonts';
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard'
+  },
+  description: 'Learning next.js',
+  metadataBase: new URL('https://static.vecteezy.com/system/resources/previews/002/002/257/non_2x/beautiful-woman-avatar-character-icon-free-vector.jpg')
+}
+
+
 export default function RootLayout({
   children,
 }: {
